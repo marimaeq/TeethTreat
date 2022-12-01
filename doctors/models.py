7dc13f1df84f49a57028bdae0bd0c322cd91b519
marimaeq <marimaeq@gmail.com>
2022-12-01 04:07:19 -0800

    trying to figure out how to add image

--- doctors/models.py
+++ doctors/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Doctor(models.Model):
     speciality = models.CharField(max_length=20)
     name = models.CharField(max_length=15)
+    photo = models.ImageField(upload_to='')
     
     # availableHours = models.
-    # photo
+    # MEDIA_ROOT/upload_to
--- doctors/urls.py
+++ doctors/urls.py
@@ -2,8 +2,15 @@
 from django.urls import path
 from .views import *
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', home_view),
     path('doctors/', doctors_list),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
